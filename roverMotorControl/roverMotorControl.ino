3f4b65a7e77aafd215e6cc4ff65a12ae345c95d6
spencercheese <77421435+spencercheese@users.noreply.github.com>
2021-05-26 00:07:20 -0700

    Updated Variables

--- roverMotorControl/roverMotorControl.ino
+++ roverMotorControl/roverMotorControl.ino
@@ -2,12 +2,12 @@
 #include <Servo.h> // Servo Motors
 #include <AFMotor.h> //DC Motors
 
-const double SECONDS_TO_TURN_90 = 10;
-const double SECONDS_TO_DRIVE_METER = 10;
+const double SECONDS_TO_TURN_90 = 1;     //<--- Change 1 to how long it takes to turn 90 degrees in seconds
+const double SECONDS_TO_DRIVE_METER = 1; //<--- Change 1 to how long it takes to drive one meter in seconds
 
-bool firstTowerDropped(false), secondTowerDropped(false); //Indicates if the towers have been dropped
+bool secondTowerDropped(false), bottomTowerDropped(false); //Indicates if the towers have been dropped
 Servo servoMotorTop, servoMotorBottom; //Constructs both Servo Motors
-int anglePositionFirst(0), anglePositionSecond(0); // Starting angles of the Servo Motors in degrees
+int anglePositionTop(0), anglePositionBottom(0); // Starting angles of the Servo Motors in degrees
 
 // Constructs all four DC motors(Parameter corresponds to the motor shield's terminal)
 AF_DCMotor frontLeftDC(1);
@@ -15,7 +15,8 @@ AF_DCMotor frontRightDC(2);
 AF_DCMotor backLeftDC(3);
 AF_DCMotor backRightDC(4);
 
-int frontLeftSpeed(0), frontRightSpeed(0), backLeftSpeed(0), backRightSpeed(0); // Speed variables for each motor
+int frontLeftStraight(100), frontRightStraight(100), backLeftStraight(100), backRightStraight(100); // <--- Change 50 to speed when driving straight(0 to 255)
+int frontLeftTurn(50), frontRightTurn(50), backLeftTurn(50), backRightTurn(50); // <--- Change 0 to speed when turning(0 to 255, probably lower than straight to prevent tipping)
 
 void setup() {
   Wire.begin(2); // Join I2C connection with address #2
@@ -27,13 +28,13 @@ void setup() {
 
   //Removes all power from all four DC motors and set speed to it's respectively speed variable
   frontLeftDC.run(RELEASE);
-  frontLeftDC.setSpeed(frontLeftSpeed);
+  frontLeftDC.setSpeed(0);
   frontRightDC.run(RELEASE);
-  frontRightDC.setSpeed(frontRightSpeed);
+  frontRightDC.setSpeed(0);
   backLeftDC.run(RELEASE);
-  backLeftDC.setSpeed(backLeftSpeed);
+  backLeftDC.setSpeed(0);
   backRightDC.run(RELEASE);
-  backRightDC.setSpeed(backRightSpeed);
+  backRightDC.setSpeed(0);
   
   Wire.onReceive(receiveEvent); // Registering event that runs when receives data from Nano
 }
@@ -42,7 +43,7 @@ void loop() {
   delay(1000);
 }
 
-void receiveEvent(int howMany) { //Commented out section for negative values if we have time
+void receiveEvent(int howMany) { //Commented out section for if we have time
   char action = Wire.read();
   int turnDegree = Wire.read();
   int driveDistance = Wire.read();
@@ -51,14 +52,15 @@ void receiveEvent(int howMany) { //Commented out section for negative values if 
     // Will turn one side of motors forward, one side backwards
     if(turnDegree > 0) { // Right(Positive) turn
       int turnScalar = turnDegree / 90;
-      frontLeftDC.setSpeed(frontLeftSpeed);
-      frontRightDC.setSpeed(frontRightSpeed);
-      backLeftDC.setSpeed(backLeftSpeed);
-      backRightDC.setSpeed(backRightSpeed);
+      frontLeftDC.setSpeed(frontLeftTurn);
+      frontRightDC.setSpeed(frontRightTurn);
+      backLeftDC.setSpeed(backLeftTurn);
+      backRightDC.setSpeed(backRightTurn);
       frontLeftDC.run(FORWARD);
       frontRightDC.run(BACKWARD);
       backLeftDC.run(FORWARD);
       backRightDC.run(BACKWARD);
+      
       delay(turnScalar * SECONDS_TO_TURN_90 * 1000);
       frontLeftDC.run(RELEASE);
       frontRightDC.run(RELEASE);
@@ -66,10 +68,10 @@ void receiveEvent(int howMany) { //Commented out section for negative values if 
       backRightDC.run(RELEASE);
     }
 //    else if(turnDegree < 0) { // Left(Negative) turn
-//      frontLeftDC.setSpeed(frontLeftSpeed);
-//      frontRightDC.setSpeed(frontRightSpeed);
-//      backLeftDC.setSpeed(backLeftSpeed);
-//      backRightDC.setSpeed(backRightSpeed);
+//      frontLeftDC.setSpeed(frontLeftTurn);
+//      frontRightDC.setSpeed(frontRightTurn);
+//      backLeftDC.setSpeed(backLeftTurn);
+//      backRightDC.setSpeed(backRightTurn);
 //      frontLeftDC.run(BACKWARD);
 //      frontRightDC.run(FORWARD);
 //      backLeftDC.run(BACKWARD);
@@ -84,10 +86,10 @@ void receiveEvent(int howMany) { //Commented out section for negative values if 
 
     if(driveDistance > 0) { // Positive drive
       double driveScalar = driveDistance / 1000;
-      frontLeftDC.setSpeed(frontLeftSpeed);
-      frontRightDC.setSpeed(frontRightSpeed);
-      backLeftDC.setSpeed(backLeftSpeed);
-      backRightDC.setSpeed(backRightSpeed);
+      frontLeftDC.setSpeed(frontLeftStraight);
+      frontRightDC.setSpeed(frontRightStraight);
+      backLeftDC.setSpeed(backLeftStraight);
+      backRightDC.setSpeed(backRightStraight);
       frontLeftDC.run(FORWARD);
       frontRightDC.run(FORWARD);
       backLeftDC.run(FORWARD);
@@ -100,10 +102,10 @@ void receiveEvent(int howMany) { //Commented out section for negative values if 
       backRightDC.run(RELEASE); 
     }
 //    else if(DRIVE DRIVE < 0) {
-//      frontLeftDC.setSpeed(frontLeftSpeed);
-//      frontRightDC.setSpeed(frontRightSpeed);
-//      backLeftDC.setSpeed(backLeftSpeed);
-//      backRightDC.setSpeed(backRightSpeed);
+//      frontLeftDC.setSpeed(frontLeftStraight);
+//      frontRightDC.setSpeed(frontRightStraight);
+//      backLeftDC.setSpeed(backLeftStraight);
+//      backRightDC.setSpeed(backRightStraight);
 //      frontLeftDC.run(BACKWARD);
 //      frontRightDC.run(BACKWARD);
 //      backLeftDC.run(BACKWARD);
@@ -117,9 +119,9 @@ void receiveEvent(int howMany) { //Commented out section for negative values if 
 //    }
   }
   else if(action == 'V') { // Tower Drop
-    if(!firstTowerDropped) { // Bottom Tower drop
+    if(!bottomTowerDropped) { // Bottom Tower drop
       servoMotorBottom.write(90);
-      firstTowerDropped = true;
+      bottomTowerDropped = true;
     }
     else if(!secondTowerDropped) { // Top Tower Drop
       servoMotorTop.write(90);
